c825b8bfd507e903a3b1c7f118b0489f97261759
Nur Chamdani <kokme.km@gmail.com>
2022-10-09 22:28:37 +0700

    update files
    updateds

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,23 @@
 
 namespace App\Controllers;
 
+use App\Models\M_Users;
+
 class Home extends BaseController
 {
     public function index()
     {
         return view('welcome_message');
+    }
+    // 
+    public function AuthLogin(){
+        $session = session();
+        $data = [];
+        if($session->get('level_id') == null){
+            $data = ['pesan','Welcome back again !'];
+            return view('login', $data);
+        }else{
+            return view('login', $data);
+        }
     }
 }
--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jurnal</title>
-    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" href="<?=site_url('/')?>css/style.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"  />
 </head>
 <body>
@@ -41,7 +41,7 @@
                     </div>
                 </div>
                 <div class="dropdown">
-                    <button class="dropbtn" id="login">LOGIN</button>
+                    <a class="dropbtn" id="login" href="<?=site_url('/login')?>">LOGIN</a>
                 </div>
             </div> 
         </div>
