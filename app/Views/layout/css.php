69dac830fa6f676d95b6f3c6fdfbd06b5aed8753
Nur Chamdani <kokme.km@gmail.com>
2022-10-09 23:07:09 +0700

    upload template adminlte
    uploaded

---
+++ app/Views/layout/css.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" href="<?=site_url('/')?>css/style.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"  />
